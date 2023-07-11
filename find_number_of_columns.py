91b100d06ae510ed4fe6ac0a1682fdd3417bf3de
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-07-11 10:34:27 +0500

    solved main

--- find_number_of_columns.py
+++ find_number_of_columns.py
@@ -1,3 +1,4 @@
+import csv 
 def find_number_of_columns(data):
     """
     Find the number of columns in CSV.
@@ -6,6 +7,16 @@ def find_number_of_columns(data):
     Return:
         int: Number of columns.
     """
-    return
-
+    lst = []
+    s = 0
+    for i in data:
+        lst.append(i) 
+    for j in lst[0]:   
+        s += 1
+        
+    return int(s)
+ 
 # Read the csv file
+f = open('data.csv')
+r = csv.reader(f)
+print(find_number_of_columns(r))
